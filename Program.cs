fae7dc2eebbd6623e8aef4d03de5081860bba458
Zienabkhalifa453 <92791041+Zienabkhalifa453@users.noreply.github.com>
2022-08-21 08:05:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    class program
+    {
+        public class Post
+        {
+            public string title;
+            public string description;
+            public DateTime DateCreatedO { get; set; }
+            public int vote { get; set; }
+            private bool vote_up;
+            private bool vote_down;
+            public Post(string Title, string Description)
+
+            {
+                title = Title;
+                description = Description;
+                DateTime DateCreatedO = DateTime.Now;
+                vote = 0;
+            }
+            public void Voteup()
+            {
+                if (vote_up)
+                {
+                    throw new Exception("You have voted");
+
+                }
+                else
+                {
+                    vote++;
+                    vote_up = true;
+                    vote_down = false;
+                } }
+            public void Votedown()
+            {
+                if (vote_down)
+                {
+                    throw new Exception("You have voted");
+
+
+                }
+                else
+                {
+                    vote--;
+                    vote_up = false;
+                    vote_down = true;
+                }
+
+
+            }
+
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter your title");
+                string t = Console.ReadLine();
+                Console.WriteLine("enter your description");
+                string d = Console.ReadLine();
+                var Post = new Post(t, d);
+                System.Console.WriteLine($"Title:{Post.title}  ");
+                System.Console.WriteLine($"Description:{Post.description} ");
+                System.Console.WriteLine($"Date Created:{Post.DateCreatedO}");
+                System.Console.WriteLine($"Post Count{Post.vote}   ");
+
+                Console.WriteLine("Enter vote up , dwn");
+
+                while (true)
+                {
+                    string inputVote =Console.ReadLine();
+                    if (inputVote.ToLower().Equals("voteup")) 
+                    {
+
+                        Post.Voteup();
+                        System.Console.WriteLine($"Post Count: {Post.vote} ");
+
+                    }
+                    else if (inputVote.ToLower().Equals("votedown")) 
+                    {
+                        Post.Votedown();
+                        System.Console.WriteLine($"Post Count: {Post.vote} ");
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("try again");
+
+                    }
+
+                }
+
+
+
+
+
+
+
+            } } 
+    }
+}
